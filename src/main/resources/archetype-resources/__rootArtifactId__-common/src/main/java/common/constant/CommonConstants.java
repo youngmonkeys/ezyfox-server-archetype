23225a3a8e53a222f5bd82877dbbfab201f54888
Dung Ta Van <itprono3@gmail.com>
2021-05-14 21:20:22 +0700

    release verison 1.1.4

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-common/src/main/java/common/constant/CommonConstants.java
@@ -0,0 +1,11 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.common.constant;
+
+public final class CommonConstants {
+
+	public static final String PLUGIN_PROPERTIES = "${package}.PluginProperties";
+
+	private CommonConstants() {}
+}
